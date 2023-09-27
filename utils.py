9f9807b62480565b5a2359258ffdd7fc2739ccdf
potomushozhenya <123594106+potomushozhenya@users.noreply.github.com>
2023-09-27 22:40:49 +0300

    Update utils.py
    Fix brand check

--- utils.py
+++ utils.py
@@ -13,12 +13,8 @@ def get_brand_list():
 #Функция для сравнения
 def brand_check(brand_list, message_text):
     get_brand_list()
-    try:
-        desired_brand = process.extractOne(message_text, list)
-        if desired_brand[1] >= 80:
-            return desired_brand
-        else:
-            message.answer(text='Бренд введен неправильно')
-    except BrandNotFoundError:
-        message_answer(text='Бренд не найден')
-    result_brand = None
+    desired_brand = process.extractOne(message_text, list)
+    if desired_brand[1] >= 80:
+        return desired_brand
+    else:
+        return None
